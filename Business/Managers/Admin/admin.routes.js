e322fd27c354c4cbade81e5b47a693e68b9b164c
Ubuntu18 <loay.el_zobaidy@emc.com>
2018-06-20 02:44:33 -0700

    authorization modified

--- Business/Managers/Admin/admin.routes.js
+++ Business/Managers/Admin/admin.routes.js
@@ -3,13 +3,16 @@ const adminManager = require('./AdminManager');
 const ideaManager = require('../Idea/IdeaManager');
 const adminValidation = require('./Config/admin.validations');
 const validate = require('express-validation');
+import config from '../../../config/config'
 import expressJwt from 'express-jwt';
+import midddlewares from './Config/admin.midddlewares';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.get('/test', (req,res)=>{
-    res.send("OK");
+router.get('/test', expressJwt({ secret: config.jwtSecret }),midddlewares.isAdmin,(req,res)=>{
+    res.json({message: "OK"})
 })
+
 router.route('/users')
-  .get(adminManager.getAllUsers);
+  .get(expressJwt({ secret: config.jwtSecret }),midddlewares.isAdmin,adminManager.getAllUsers);
 module.exports = router;
